023a7cc833d9a3ddac05364fbe5d5a1a56afd62e
Emmakki <emmivdc@gmail.com>
2023-10-18 11:54:17 +0200

    Init commit

---
+++ SOFT_DECODER_GROUPE3.m
@@ -0,0 +1,10 @@
+
+% Fonction de décodage LDPC: Soft decision
+% Paramètres:
+% c, un vecteur colonne binaire de dimension [N, 1]
+% H, une matrice de parité de dimension [M, N] constituée de true et false
+% p, un vecteur colonne de dimension [N, 1]
+% MAX_ITER, un entier strictement positif
+% Sortie:
+% c_cor le vecteur colonne binaire de dimension [N, 1]
+SOFT_DECODER_GROUPE3(c, H, p, MAX_ITER)
