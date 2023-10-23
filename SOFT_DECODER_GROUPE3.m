05803e3bb5de928a6beb1b2241e9cfbd743fc2f0
Emmakki <emmivdc@gmail.com>
2023-10-23 15:27:19 +0200

    Commentaires et signature de la fonction

--- SOFT_DECODER_GROUPE3.m
+++ SOFT_DECODER_GROUPE3.m
@@ -1,10 +1,17 @@
+%% SOFT_DECODER_GROUPE3.m
+% ========================================================================
+% Author: Bibard Grau Emma; Beny Guillaume; Bek Nada; Elmoujaouid Boutaina; 
+% Date: 2023, Octobre
+% ========================================================================
 
-% Fonction de décodage LDPC: Soft decision
-% Paramètres:
-% c, un vecteur colonne binaire de dimension [N, 1]
-% H, une matrice de parité de dimension [M, N] constituée de true et false
-% p, un vecteur colonne de dimension [N, 1]
-% MAX_ITER, un entier strictement positif
-% Sortie:
+function c_cor = SOFT_DECODER_GROUPE3(c, H, p, MAX_ITER)
+% SOFT_DECODER_GROUPE3 Fonction de décodage LDPC: Soft decision
+% 
+% c : un vecteur colonne binaire de dimension [N, 1]
+% H : une matrice de parité de dimension [M, N] constituée de true et false
+% p : un vecteur colonne de dimension [N, 1]
+% MAX_ITER : un entier strictement positif
+% 
 % c_cor le vecteur colonne binaire de dimension [N, 1]
-SOFT_DECODER_GROUPE3(c, H, p, MAX_ITER)
+
+end
